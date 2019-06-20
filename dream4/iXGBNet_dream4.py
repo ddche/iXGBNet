68294641c0ae7e9ce769805294367ad3d17b40fb
cdd <cdd@maqiangs-MacBook-Pro-2.local>
2019-06-20 11:02:53 +0800

    commit

--- dream4/iXGBNet_dream4.py
+++ dream4/iXGBNet_dream4.py
@@ -26,7 +26,7 @@ def main(data_tm, sample_num, k, alpha, iter_num, data_ko):
     time_start = time.time()
 
     # normalize data_tm
-    data_tm = normalized_zscore(data_tm)
+    # data_tm = normalized_zscore(data_tm)
 
     # Compute the accumulation of previous time points for time series data.
     x, y = time_accumu(data_tm, sample_num, k, alpha)
